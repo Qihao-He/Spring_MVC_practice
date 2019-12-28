a8b6735b681ff0a795b5a0480681af97df1638ab
Qihao He <qi.he@maine.edu>
2019-12-27 21:56:07 -0500

    debug for wrong directory

--- src/main/java/com/telusko/AddController.java
+++ src/main/java/com/telusko/AddController.java
@@ -1,5 +1,8 @@
 package com.telusko;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,7 +13,7 @@ import com.telusko.service.AddService;
 public class AddController {
 //	@RequestMapping(path = "/add", method = RequestMethod.GET)
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response, ) {
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
 //		System.out.println("add controller working");
 		int i = Integer.parseInt(request.getParameter("t1"));
 		int j = Integer.parseInt(request.getParameter("t2"));
