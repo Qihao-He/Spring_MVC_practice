5e849d4433f0c0ccfc7c4995fa2cd8d38ac71afe
Qihao He <qi.he@maine.edu>
2019-12-27 21:06:39 -0500

    sum adder httpServletRequest httpServletResponse

--- src/main/resources/com/telusko/AddController.java
+++ src/main/resources/com/telusko/AddController.java
@@ -2,13 +2,21 @@ package com.telusko;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
 //	@RequestMapping(path = "/add", method = RequestMethod.GET)
 	@RequestMapping("/add")
-	public String add() {
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response, ) {
 //		System.out.println("add controller working");
-		return "display.jsp";
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		int k = i + j;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result",k);
+		return mv;
 	}
 }
--- src/main/webapp/display.jsp
+++ src/main/webapp/display.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	A new page, Im here.
+	Result is: <%= request.getAttribute("result") %>>
 </body>
 </html>
