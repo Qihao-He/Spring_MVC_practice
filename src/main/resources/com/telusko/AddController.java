7bde766c1cbd78d1e04220bc73cf391f5fbf34eb
Qihao He <qi.he@maine.edu>
2019-12-27 21:01:00 -0500

    display page added

--- src/main/resources/com/telusko/AddController.java
+++ src/main/resources/com/telusko/AddController.java
@@ -1,7 +1,14 @@
 package com.telusko;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AddController {
-	public void add() {
-		System.out.println("add controller working");
+//	@RequestMapping(path = "/add", method = RequestMethod.GET)
+	@RequestMapping("/add")
+	public String add() {
+//		System.out.println("add controller working");
+		return "display.jsp";
 	}
 }
