f49116f8eca4d786cc22c447001413387028482e
Qihao He <qi.he@maine.edu>
2019-12-27 22:43:27 -0500

    spring mvc annotation requestParam

--- src/main/java/com/telusko/AddController.java
+++ src/main/java/com/telusko/AddController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.telusko.service.AddService;
@@ -12,9 +13,9 @@ import com.telusko.service.AddService;
 @Controller
 public class AddController {
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
-		int i = Integer.parseInt(request.getParameter("t1"));
-		int j = Integer.parseInt(request.getParameter("t2"));
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j, HttpServletRequest request, HttpServletResponse response) {
+//		int i = Integer.parseInt(request.getParameter("t1"));
+//		int j = Integer.parseInt(request.getParameter("t2"));
 		
 		AddService as = new AddService();
 		int sum = as.add(i, j);
