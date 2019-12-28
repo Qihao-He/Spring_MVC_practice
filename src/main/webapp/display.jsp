4d9c0fd635b0de005a0e56dad9f6f8697a71d8e8
Qihao He <qi.he@maine.edu>
2019-12-27 21:37:37 -0500

    move add logic operation from controller to service class

--- src/main/webapp/display.jsp
+++ src/main/webapp/display.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Result is: <%= request.getAttribute("result") %>>
+	<%-- Result is: <%= request.getAttribute("result") %>> --%>
+	Result is: ${result}
 </body>
 </html>
--- target/classes/com/telusko/AddController.java
+++ target/classes/com/telusko/AddController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.telusko.service.AddService;
+
 @Controller
 public class AddController {
 //	@RequestMapping(path = "/add", method = RequestMethod.GET)
@@ -12,11 +14,13 @@ public class AddController {
 //		System.out.println("add controller working");
 		int i = Integer.parseInt(request.getParameter("t1"));
 		int j = Integer.parseInt(request.getParameter("t2"));
-		int k = i + j;
+		
+		AddService as = new AddService();
+		int sum = as.add(i, j);
 		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("display.jsp");
-		mv.addObject("result",k);
+		mv.addObject("result", sum);
 		return mv;
 	}
 }
